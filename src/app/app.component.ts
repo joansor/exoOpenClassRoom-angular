96442cc2fbd45fa6858713a66e8cbd9c7dc2620b
joansor <sor.joan@gmail.com>
2020-03-25 21:36:56 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'exoOpenClassRoom-angular';
+
+  export class AppComponent {
+    isAuth = false;
+    
+    appareilOne = 'Machine à laver';
+    appareilTwo = 'Frigo';
+    appareilThree = 'Ordinateur';
+  
+    
+  constructor() {
+
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+  onAllumer() {
+    console.log('On allume tout !');
+  }
 }
+
